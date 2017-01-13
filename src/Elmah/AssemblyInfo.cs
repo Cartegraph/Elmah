09bb244209f6fdc0d86381ed032d3f1e53dde753
Kevin <kevinkelchen@cartegraph.com>
2017-01-13 12:10:01 -0600

    Incrementing the assembly version. This should help us identify the version of Elmah being referenced/used and the changes it contains.

--- src/Elmah/AssemblyInfo.cs
+++ src/Elmah/AssemblyInfo.cs
@@ -37,8 +37,8 @@ using ComVisible = System.Runtime.InteropServices.ComVisibleAttribute;
 [assembly: AssemblyCopyright("Copyright (c) 2004, Atif Aziz. All rights reserved.")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.2.14706.0")]
-[assembly: AssemblyFileVersion("1.2.14706.0955")]
+[assembly: AssemblyVersion("1.2.14707.0")]
+[assembly: AssemblyFileVersion("1.2.14707.0000")]
 [assembly: AssemblyConfiguration(Elmah.Build.Configuration)]
 
 [assembly: CLSCompliant(true)] 
